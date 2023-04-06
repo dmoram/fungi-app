f84ef73656ce2c5e479d8f24092510434875ee64
Diegomora1 <diegomora940mx@gmail.com>
2023-04-05 23:00:08 -0400

    tab navigator base

---
+++ src/components/FungiSensor/FungiSensorScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FungiSensorScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>FungiSensorScreen</Text>
+    </View>
+  )
+}
+
+export default FungiSensorScreen
+
+const styles = StyleSheet.create({})
--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -1,16 +1,64 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FeedScreen from '../components/Feed/FeedScreen';
 import ProfileScreen from '../components/Profile/ProfileScreen';
+import FungiSensorScreen from '../components/FungiSensor/FungiSensorScreen';
+import { StyleSheet } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Tab = createBottomTabNavigator();
+
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+const Tab = createMaterialBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Feed" component={FeedScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Navigator 
+      activeColor="#f0edf6"
+      inactiveColor="#3e2465"
+      barStyle={{ backgroundColor: 'green' }}
+    >
+      <Tab.Screen 
+        name="Feed" 
+        component={FeedScreen}
+        options={{
+          tabBarLabel: 'Feed',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={34} />
+          ),
+        }}   
+        
+      />
+      <Tab.Screen 
+        name="Fungi" 
+        component={FungiSensorScreen}
+        options={{
+          tabBarLabel: 'Fungi',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="mushroom" color={color} size={34} />
+          ),
+        }}   
+        
+      />
+
+      <Tab.Screen 
+        name="Profile" 
+        component={ProfileScreen} 
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={34} />
+          ),
+        }}  
+      />
     </Tab.Navigator>
   );
 }
 
 export default MyTabs;
+
+const styles = StyleSheet.create({
+  tabs: {
+    backgroundColor: 'green'
+  }
+});
+
