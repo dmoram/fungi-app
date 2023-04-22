55b73491c5561a962f294653059939a61b29017d
Diegomora1 <diegomora940mx@gmail.com>
2023-04-22 03:31:18 -0400

    nice

--- src/screens/NewPostScreen.js
+++ src/screens/NewPostScreen.js
@@ -22,11 +22,8 @@ const NewPostScreen = ({ navigation }) => {
   const [image, setImage] = useState(null);
 
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
       quality: 1,
     });
 
